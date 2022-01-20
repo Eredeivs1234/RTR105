012e82659f1408cbb4fa4842e20d4f18561e24d1
Dāvids Ērenpreiss <90239525+Eredeivs1234@users.noreply.github.com>
2022-01-20 22:13:49 +0200

    Update ld3.c

--- ld3_derivative/ld3.c
+++ ld3_derivative/ld3.c
@@ -11,19 +11,19 @@ void main() {
     printf("\nLūdzu ievadiet precizitātes vērtību: ");
     scanf("%lf", &c);
 
-    printf("\n        x            j1(-x)          j1\'(-x)            j1\'(-x)             j1\'\'(-x)          j1\'\'(-x)  \n"
+    printf("\n        x            j1(x)          j1\'(x)            j1\'(x)             j1\'\'(x)          j1\'\'(x)  \n"
            "                                analytical formula  finite difference analytical formula finite difference\n");
     printf("\n");
     
    x = a;
    while(x < b){
       
-       aa=(j1(-x));
+       aa=(j1(x));
        ab=(1/(pow(-x,2)+1));
-       ac=(j1(-x)-j1(-x-c))/c;
-       ad=(-2*(-x))/(pow(-x,4)+2*pow(-x,2)+1);
-       f1=(1/(1+pow(-x+c,2)));
-       f2=(1/(1+pow(-x,2)));
+       ac=(j1(x)-j1(-x-c))/c;
+       ad=(-2*(x))/(pow(x,4)+2*pow(-x,2)+1);
+       f1=(1/(1+pow(x+c,2)));
+       f2=(1/(1+pow(x,2)));
        ae=((f1-f2)/c);
        
        printf(" %10.3f\t%10.3f\t %10.3f\t   %10.3f\t       %10.3f\t   %10.3f\n",x,aa,ab,ac,ad,ae);
